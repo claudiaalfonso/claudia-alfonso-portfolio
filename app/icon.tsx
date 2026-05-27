5b010e1fa382427dca1ad86188d5b670a8d9e4c5
claudiaalfonso <93072940+claudiaalfonso@users.noreply.github.com>
2026-05-27 17:50:25 +0200

    Refine round favicon crop

--- app/icon.tsx
+++ app/icon.tsx
@@ -39,12 +39,13 @@ export default async function Icon() {
       >
         <div
           style={{
-            width: '100%',
-            height: '100%',
+            width: '86%',
+            height: '86%',
             display: 'flex',
             overflow: 'hidden',
             background: '#b8c8c3',
             borderRadius: '9999px',
+            border: '1px solid rgba(255,255,255,0.85)',
           }}
         >
           <img
